f77358e6358178e2f4d8063dc18300c624bc76c0
Sanjeev Singh <sanjeevsinghsaini48@gmail.com>
2024-03-14 21:39:45 +0530

    menu created

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,14 @@
-import { useState } from "react"
-import {motion} from "framer-motion";
-import { Input } from "./components/InputComponent/Input";
-const App = () => {
-   const [x,setX]=useState(0);
-   const [y,setY]=useState(0);
-   const [rotate,setRotate]=useState(0);
-  return (
-    <div className="example">
-      <div>
-        <motion.div
-        className="box"
-        animate={{x,y,rotate}}
-        transition={{type:'spring'}}
-        />
-      </div>
+import React from 'react'
+import Menu from './pages/menu/Menu'
 
-      <div className="inputs">
-        <Input value={x} set={setX}>
-          x
-        </Input>
-        <Input value={y} set={setY}>
-          y
-        </Input>
-        <Input value={rotate} set={setRotate} min={-180} max={180}>
-          rotate
-        </Input>
-      </div>
+
+
+const App = () => {
+  return (
+    <div>
+        <Menu/>
     </div>
-  );
+  )
 }
 
 export default App
